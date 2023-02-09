13073740dfbe65c6e480ce1eeae7013a0d044e70
Said WAHID <said@oumdin.com>
2023-02-09 07:49:06 +0100

    Type Record

---
+++ NouveautesCSharp9/TypesRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypesRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public record Personne(string FirstName, string LastName);
+
+    public record Client
+    {
+        public string FirstName { get; init; } = default!;
+        public string LastName { get; init; } = default!;
+    };
+
+    public record Fournisseur
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+    };
+
+
+}
